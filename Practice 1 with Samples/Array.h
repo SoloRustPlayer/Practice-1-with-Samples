d0887215a28d7c40c12bab6bf2069a240f918de8
SoloRustPLayer <savaignatov333@gmail.com>
2024-12-10 19:25:23 +0200

    Added constructors and destructor

--- Practice 1 with Samples/Array.h
+++ Practice 1 with Samples/Array.h
@@ -11,6 +11,11 @@ public:
 	Array(size_t n);
 	Array(size_t n, int min, int max);
 	~Array();
-
+	void display() const;
+	void init(int min, int max);
+	void resize(size_t newSize);
+	int findMin() const;
+	int findMax() const;
+	void sort();
 
 };
